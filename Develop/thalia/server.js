29650b08d34d6cbf8d2f48d2f3fb732486e31019
Thalia Rodriguez <thaliarpuron@gmail.com>
2020-10-07 21:04:50 -0400

    html routes

--- Develop/thalia/server.js
+++ Develop/thalia/server.js
@@ -1,1 +1,28 @@
-const hello = 'hello world';
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const server = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+server.use(express.static("public"));
+
+// Parse application body as JSON
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+server.engine("handlebars", exphbs({ defaultLayout: "main" }));
+server.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgers_controller.js");
+
+server.use(routes);
+
+server.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
