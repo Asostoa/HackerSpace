0a61ab22473d4ee3200a26622c6e635592fe12a4
Thalia Rodriguez <thaliarpuron@gmail.com>
2020-10-11 13:12:59 -0400

    everything changed

--- Develop/thalia/routes/html-routes.js
+++ Develop/thalia/routes/html-routes.js
@@ -8,30 +8,22 @@ module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/userProfile");
+      res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/userProfile");
+      res.redirect("/members");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
-  app.get("/signup", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/userProfile");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
-
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/userProfile", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/userProfile.html"));
+  app.get("/members", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 };
