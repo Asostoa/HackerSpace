a3bd7eb3bec3781590f27cb5df4cbd6feaf94bbf
Miguelangel Weill <miguelangel.weill@gmail.com>
2020-10-12 20:25:50 -0400

    Lets see what this does

--- Develop/miguel/public/index.js
+++ Develop/miguel/public/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+$("#uploadBtn").click(function(event){
+    event.preventDefault();
+    var input = document.querySelector("input[type=file]"),
+      file = input.files[0];
+    console.log(file)
 
 })
+
+
+});
