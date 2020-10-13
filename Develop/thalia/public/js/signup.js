9d07302a62ffb12062baa07470ba41fe40b6fb52
Thalia Rodriguez <thaliarpuron@gmail.com>
2020-10-12 22:45:25 -0400

    working perfectly fine :)

--- Develop/thalia/public/js/signup.js
+++ Develop/thalia/public/js/signup.js
@@ -4,6 +4,10 @@ $(document).ready(() => {
   const nameInput = $("input#name-input");
   const emailInput = $("input#email-input");
   const passwordInput = $("input#password-input");
+  const cityInput = $("input#city-input");
+  const technologyInput = $("input#technology-input");
+  const githubInput = $("input#github-input");
+  const linkedinInput = $("input#linkedin-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", event => {
@@ -11,27 +15,39 @@ $(document).ready(() => {
     const userData = {
       name: nameInput.val().trim(),
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: passwordInput.val().trim(),
+      city: cityInput.val().trim(),
+      technology: technologyInput.val().trim(),
+      github: githubInput.val().trim(),
+      linkedin: linkedinInput.val().trim(),
     };
 
-    if (!userData.name || !userData.email || !userData.password) {
+    if (!userData.name || !userData.email || !userData.password || !userData.city || !userData.technology || !userData.github, !userData.linkedin) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.name, userData.email, userData.password);
+    signUpUser(userData.name, userData.email, userData.password, userData.city, userData.technology, userData.github, userData.linkedin);
     nameInput.val("");
     emailInput.val("");
     passwordInput.val("");
-    
+    cityInput.val("");
+    technologyInput.val("");
+    githubInput.val("");
+    linkedinInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(name, email, password) {
+  function signUpUser(name, email, password, city, technology, github, linkedin) {
     $.post("/api/signup", {
       name: name,
       email: email,
-      password: password
+      password: password,
+      city: city,
+      technology: technology,
+      github: github,
+      linkedin: linkedin,
+
     })
       .then(() => {
         window.location.replace("/members");
