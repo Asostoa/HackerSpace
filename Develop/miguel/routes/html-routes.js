e67a50b07e5271ca100969ede94105422b5e1f54
Miguelangel Weill <miguelangel.weill@gmail.com>
2020-10-14 20:48:18 -0400

    Most update

--- Develop/miguel/routes/html-routes.js
+++ Develop/miguel/routes/html-routes.js
@@ -26,9 +26,4 @@ module.exports = function(app) {
   app.get("/members", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
-
-  app.get("/uploads", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/userProfile.html"));
-  });
-
 };
